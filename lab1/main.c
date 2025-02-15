efb01704b2e9d1974a14194805e75843b3d72f8e
EgorSkeleton <113343110+EgorSkeleton@users.noreply.github.com>
2025-02-15 12:58:35 +1000

    Add files via upload

--- lab1/main.c
+++ lab1/main.c
@@ -8,15 +8,23 @@ int main(int argc, char *argv[]) {
 	double a, b, c;
 	double x1, x2, d;
 	a = 5.3;
-	b = 25.1;
+	b = 0.1;
 	c = 6.0;
-	
 	d = pow(b, 2) - 4*a*c;
-	x1 = (-b + sqrt(d))/(2*a);
-	x2 = (-b - sqrt(d))/(2*a);
-	printf("x1 = %lf\n", x1);
-	printf("x2 = %lf\n", x2);
-	printf("d = %lf", d);
+	printf("d = %lf\n", d);
 	
+	if(d < 0) printf("KORNEI NET");
+	if(d > 0) {
+		d = pow(b, 2) - 4*a*c;
+		x1 = (-b + sqrt(d))/(2*a);
+		x2 = (-b - sqrt(d))/(2*a);
+		printf("x1 = %lf\n", x1);
+		printf("x2 = %lf\n", x2);
+		printf("d = %lf", d);
+	}
+	if(d == 0){
+		x1 = -b/(2*a);
+		printf("Only one koren = %lf\n", x1);
+	}
 	return 0;
 }
