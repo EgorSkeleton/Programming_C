10685ca5d1b6db23f79fa6c985536eb8ce9575f8
EgorSkeleton <113343110+EgorSkeleton@users.noreply.github.com>
2025-03-04 18:05:08 +1000

    Add files via upload

--- lab1/task2.c
+++ lab1/task2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void){
+void main(){
 	int i, j;
 	double list_1[3][3];
 	for (i = 0; i < 3; i++){
@@ -10,13 +10,13 @@ int main(void){
 			scanf("%lf", &list_1[i][j]);
 		}
 	}
-	sum_on_main_diaganal = list_1[0][0] + list_1[1][1] + list_1[2][2];
+	int sum_on_main_diaganal = list_1[0][0] + list_1[1][1] + list_1[2][2];
 	printf("Summa na glavnoi diagonali: %d", sum_on_main_diaganal);
-	sum_on_side_diaganal = list_1[0][2] + list_1[1][1] + list_1[2][0];
+	int sum_on_side_diaganal = list_1[0][2] + list_1[1][1] + list_1[2][0];
 	printf("Summa na pobochnoi diagonali: %d", sum_on_side_diaganal);
 	int list_2[2][2];
-	for (i=0, i<2, i++){
-		for (j=0, j<2, j++){
+	for (i=0; i<2; i++){
+		for (j=0; j<2; j++){
 			printf("Vvod: ");
 			scanf("%lf", &list_2[i][j]);
 		}
@@ -26,6 +26,4 @@ int main(void){
 	list_2_sq[0][1] = list_2[0][0] * list_2[0][1] + list_2[0][1] * list_2[1][1];
 	list_2_sq[1][0] = list_2[1][0] * list_2[0][0] + list_2[1][1] * list_2[1][0];
 	list_2_sq[1][1] = list_2[1][0] * list_2[0][1] + list_2[1][1] * list_2[1][1];
-	}
-	return 0;
 }
