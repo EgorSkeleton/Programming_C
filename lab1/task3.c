654093f8e73392807516fae46951820c7ab6134d
EgorSkeleton <113343110+EgorSkeleton@users.noreply.github.com>
2025-03-04 19:34:24 +1000

    Add files via upload

--- lab1/task3.c
+++ lab1/task3.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
 
+void main(){
+	char str[80];
+	int i;
+	printf("Vvedite stroku simvolov: ");
+	fgets(str, sizeof(str), stdin);
+	for (i = 0; i<sizeof(str); i++){
+		char ch = str[i];
+		if ((ch == 'a')||(ch == 'b')){
+			str[i] = toupper(ch);
+		}
+	}
+	printf("Result: %s\n", str);
+}
