e3b611e03031d22c0efb2fe22e42ac43ef273832
EgorSkeleton <113343110+EgorSkeleton@users.noreply.github.com>
2025-03-15 12:06:19 +1000

    Add files via upload

--- lab1/task4/task4.c
+++ lab1/task4/task4.c
@@ -1,1 +1,22 @@
+#include "task4.h"
+
+int main() {
+	int a, b, c;
+	printf("Vvedite a: ");
+	scanf("%d", &a);
+	printf("Vvedite b: ");
+	scanf("%d", &b);
+	printf("Vvedite c: ");
+	scanf("%d", &c);
+	if (!triangle_rool(a, b, c)){
+		return 0;
+	}
+	int P = perimeter(a, b, c);
+	int p = P/2;
+	int S = square(a, b, c, p);
+	printf("Perimetr trugolnika: %d\n", P);
+	printf("Ploshad treugolnika: %d\n", S);
+	return 0;
+}
+
 
