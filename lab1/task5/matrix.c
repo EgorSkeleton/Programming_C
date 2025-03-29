7b01e3879a42089a03a5e238be9d0165f867a5b3
EgorSkeleton <113343110+EgorSkeleton@users.noreply.github.com>
2025-03-29 13:17:02 +1000

    Add files via upload

---
+++ lab1/task5/matrix.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double** matrix(int n){
+	double **arr;
+	int i, k;
+	arr = (double**)malloc(n * sizeof(double*));
+	for (i = 0; i < n; i++){
+		arr[i] = (double*)malloc(n * sizeof(double));
+	}
+	for (i = 0; i < n; i++){
+		for (k = 0; k < n; k++){
+			printf("Vvedite element masiva: ");
+			scanf("%lf", &arr[i][k]);
+		}
+	}
+	return arr;
+}
+
+double** sum(double **arr1, double **arr2, int n){
+	double **res;
+	int i, j;
+	res = (double**)malloc(n * sizeof(double*));
+	for (i = 0; i < n; i++){
+		res[i] = (double*)malloc(n * sizeof(double));
+	}
+	for (i=0; i<n; i++){
+		for(j=0; j<n; j++){
+			res[i][j] = arr1[i][j] + arr2[i][j];
+		}
+	}
+	return res;
+}
+
+double** diff(double **arr1, double **arr2, int n){
+	double **res;
+	int i, j;
+	res = (double**)malloc(n * sizeof(double*));
+	for (i = 0; i < n; i++){
+		res[i] = (double*)malloc(n * sizeof(double));
+	}
+	for (i=0; i<n; i++){
+		for(j=0; j<n; j++){
+			res[i][j] = arr1[i][j] - arr2[i][j];
+		}
+	}
+	return res;
+}
+
+double** mult(double **arr1, double **arr2, int n){
+	double **res;
+	int i, j, k;
+	res = (double**)malloc(n * sizeof(double*));
+	for (i = 0; i < n; i++){
+		res[i] = (double*)malloc(n * sizeof(double));
+	}
+	for (i=0; i<n; i++){
+		for(j=0; j<n; j++){
+			for(k=0; k<n; k++){
+				res[i][j] += arr1[i][k] * arr2[k][j];
+			}
+		}
+	}
+	return res;
+}
---
+++ lab1/task5/matrix.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double** matrix(int n);
+double** sum(double **arr1, double **arr2, int n);
+double** diff(double **arr1, double **arr2, int n);
+double** mult(double **arr1, double **arr2, int n);
