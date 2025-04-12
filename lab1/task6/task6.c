f8c40a1f0ede16ea3265d8bebfeb9f51da8accd1
EgorSkeleton <113343110+EgorSkeleton@users.noreply.github.com>
2025-04-12 13:25:36 +1000

    Add files via upload

--- lab1/task6/task6.c
+++ lab1/task6/task6.c
@@ -3,6 +3,7 @@
 
 int main() {
 	FILE *ln, *newf;
+	int k;
 	ln = fopen("Lastnames.txt", "rt");
 	newf = fopen("After1980.txt", "w");
 	if (ln == NULL){
@@ -11,20 +12,30 @@ int main() {
 	}
 	char str[50];
 	int i, age;
+	printf("Choose sort type: 1 - by 1980, 2 - by 'a' ");
+	scanf("%d", &k);
 	while(fgets(str, 50, ln)!=NULL){
-		for(i=0; i<51; i++){
-			printf("%c", str[i]);
-			if (str[i] == '1' || str[i] == '2'){
-				char a[5] = {str[i], str[i+1], str[i+2], str[i+3], '\0'};
-				printf("%s", a);
-				age = atoi(a);
-				if (age > 1980){
-					fputs(str, newf);
+		switch(k){
+			case(1):
+					for(i=0; i<51; i++){
+					if (str[i] == '1' || str[i] == '2'){
+						char a[5] = {str[i], str[i+1], str[i+2], str[i+3], '\0'};
+						printf("%s", a);
+						age = atoi(a);
+						if (age > 1980){
+							fputs(str, newf);
+						}
+						break;
+					}
 				}
-			break;
+				case(2):
+					if(str[i] == 'A'){
+						fputs(str, newf);
+					}
 			}
-		}
+			
 	}
+		
 	fclose(ln);
 	fclose(newf);
 	return 0;
