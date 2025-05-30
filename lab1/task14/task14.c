2b94b75e85e9dfcf99ff281c266cea1df9f5853c
EgorSkeleton <113343110+EgorSkeleton@users.noreply.github.com>
2025-05-30 19:33:21 +1000

    Add files via upload

--- lab1/task14/task14.c
+++ lab1/task14/task14.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <setjmp.h>
 
+jmp_buf env;
+unsigned long long sum = 0;
+
+int fib(unsigned long long num1, unsigned long long num2, int amount){
+	sum += num1;
+	
+	if(amount == 1){
+		longjmp(env, 1);
+	}
+	
+	return fib(num2, num2+num1, amount-1);
+}
+
+int main(){
+	int am;
+	printf("Input numbers amount: ");
+	scanf("%d", &am);
+	int val = setjmp(env);
+	
+	if(val == 0){
+		fib(1, 1, am);
+	}
+	
+	printf("%llu", sum);
+	return 0;
+}
