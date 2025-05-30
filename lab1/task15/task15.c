2264ae8bcf0fe53e3fb1b9b26c1a275adc0bc140
EgorSkeleton <113343110+EgorSkeleton@users.noreply.github.com>
2025-05-30 19:33:54 +1000

    Add files via upload

--- lab1/task15/task15.c
+++ lab1/task15/task15.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void forward(int N){
+	if (N<10){
+		printf("%d ", N);
+	}else{
+		forward(N / 10);
+		printf("%d ", N % 10);
+	}
+}
+
+void backward(int N){
+	if (N<10){
+		printf("%d ", N);
+	}else{
+		printf("%d ", N % 10);
+		backward(N / 10);
+	}
+}
+
+int main() {
+	int N;
+	printf("Input natural number: ");
+	scanf("%d", &N);
+	forward(N);
+	printf("\n");
+	backward(N);
+	return 0;
+}
